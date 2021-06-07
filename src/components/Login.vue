6e0b6a7e70b00790d002fdbe5a39f7bd4876d1db
Lara Graziela <laragdiogo@gmail.com>
2021-06-07 14:57:31 -0300

    att

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <h3>LOGIN</h3>
+    <label for="email">Email:</label> <br />
+    <input v-model="email" type="email" /><br />
+
+    <label for="senha">Senha:</label><br />
+    <input v-model="senha" type="password" /> <br /><br />
+    <button @click="login" target="_blank">ENTRAR</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: "",
+    senha: "",
+  }),
+  methods: {
+    login() {
+      if (this.email == "lara" && this.senha == "123") {
+        let route = this.$router.resolve({ path: "../views/Perfil.vue" });
+        window.open(route.href, "_blank");
+      } else {
+        console.log("erro");
+      }
+      console.log(this.email);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  box-shadow: 1px 1px 10px 1px rgb(202, 202, 202);
+}
+</style>
